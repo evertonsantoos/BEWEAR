4dff9a22aad64311aa01c0a68b68cb6377a9449d
Everton <evertoncefal1@gmail.com>
2025-08-10 11:15:29 -0300

    feat: add category products page

--- src/components/common/category-selector.tsx
+++ src/components/common/category-selector.tsx
@@ -1,3 +1,5 @@
+import { Link } from "lucide-react";
+
 import { categoryTable } from "@/db/schema";
 
 import { Button } from "../ui/button";
@@ -16,7 +18,7 @@ const CategorySelector = ({ categories }: CategorySelectorProps) => {
             variant="ghost"
             className="rounded-full bg-white text-xs font-semibold"
           >
-            {category.name}
+            <Link href={`/category/${category.slug}`}></Link>
           </Button>
         ))}
       </div>
